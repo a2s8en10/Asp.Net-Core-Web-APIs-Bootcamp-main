39f6826376f306c3ffcb7eea9dba6b6d18f8972f
Anurag Sahu <anu.sahu9589@gmail.com>
2024-07-15 23:25:08 +0530

    Update

---
+++ ConsoleApp1/Controller/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp1.Controller
+{
+    [ApiController]
+    [Route("Test")]
+    public class TestController : ControllerBase
+    {
+        public string getall()
+        {
+            return "Get all";
+        }
+       
+    }
+}
